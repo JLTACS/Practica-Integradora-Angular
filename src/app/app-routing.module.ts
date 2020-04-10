c28a5fb8ba9c63b90bd5a1520f7d3ab6b02f6880
Jose Luis Torrentera <jltorrenterash@gmail.com>
2020-04-10 01:13:20 -0500

    Correcting absolute  routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from 'src/home/home.component';
-import { ProductMainComponent } from 'src/productos/product-main/product-main.component';
-import { ProductDetailComponent } from 'src/productos/product-main/product-detail/product-detail.component';
-import { ProductEditComponent } from 'src/productos/product-main/product-edit/product-edit.component';
-import { ProductListComponent } from 'src/productos/product-main/product-list/product-list.component';
-import { NotFoundComponent } from 'src/not-found/not-found.component';
+import { HomeComponent } from '../home/home.component';
+import { ProductMainComponent } from '../productos/product-main/product-main.component';
+import { ProductDetailComponent } from '../productos/product-main/product-detail/product-detail.component';
+import { ProductEditComponent } from '../productos/product-main/product-edit/product-edit.component';
+import { ProductListComponent } from '../productos/product-main/product-list/product-list.component';
+import { NotFoundComponent } from '../not-found/not-found.component';
 
 
 const routes: Routes = [
